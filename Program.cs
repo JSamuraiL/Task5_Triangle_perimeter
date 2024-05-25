b1af448f35f61fbfa41f0102d5028ef5b148298e
JSamuraiL <alex.ermiloff2017@yandex.ru>
2024-05-25 22:22:07 +0300

    Добавлена обработка неизвестной ошибки

--- Program.cs
+++ Program.cs
@@ -5,5 +5,6 @@ using Task5;
 try {var Triangle = new Triangle(3, 4, 5);}
 
 catch (NonExistentTriangleException e) { Console.WriteLine(e.Message); }
+catch (Exception) { Console.WriteLine("Неизвестная ошибка"); }
 
 finally { Console.WriteLine("Работа закончена"); }
