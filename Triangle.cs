7ef9f7fadd4cbecb4f1200955bf0903a0ace6d58
JSamuraiL <alex.ermiloff2017@yandex.ru>
2024-05-25 20:20:06 +0300

    Создание треугольника

---
+++ Triangle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    public class Triangle
+    {
+        public float firstside {  get; set; }
+        public float secondside { get; set; }
+        public float thirdside { get; set; }
+    }
+}
